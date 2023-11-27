5e764557bb23984536dd539bc2fe2fe55a667c5d
ThrowsGG <throwsgg@gmail.com>
2023-11-27 22:29:46 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/orgrmsoft/OrgRmsoftApplication.java
@@ -0,0 +1,13 @@
+package com.example.orgrmsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgRmsoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrgRmsoftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/orgrmsoft/OrgRmsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orgrmsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgRmsoftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
